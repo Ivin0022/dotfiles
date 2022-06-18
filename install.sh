c37422252037f15a077393ac964236711562434b
ivin stephen <ivinstephen001@gmail.com>
2022-06-18 17:49:58 +0530

    install

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
-echo "hello world"
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y -qq git zsh fzf autojump
+
+# nerd fonts
+# https://github.com/ryanoasis/nerd-fonts
+sh -c "$(wget https://raw.github.com/ryanoasis/nerd-fonts/master/install.sh -O-)"
+
+
+# oh my zsh 
+# https://github.com/ohmyzsh/ohmyzsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O-)"
